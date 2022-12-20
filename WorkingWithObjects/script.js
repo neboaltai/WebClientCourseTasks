23d4bdc4c4eca998dc10750827352fd0f5e699c8
Mikhail Blinov <spasibo1989@gmail.com>
2022-12-20 22:15:54 +0700

    Исправлены замечания от 19.12.22 г. 1. Отформатирован код 2. Заменены стрелочные функции на анонимные 3. Изменены имена переменных 4. Исправлена функция getCountriesWithCitiesMaxCount() - максимум вычисляется 1 раз 5. Заменена функция getPopulationByCountry() методом forEach()

--- WorkingWithObjects/script.js
+++ WorkingWithObjects/script.js
@@ -1,52 +1,61 @@
 ﻿(function () {
-    function getCountriesWithMaxCountOfCities(countries) {
-        return countries.filter(country =>
-               country.cities.length === countries.reduce((maxCount, country) =>
-               Math.max(maxCount, country.cities.length), 0));
-    }
+    function getCountriesWithCitiesMaxCount(countries) {
+        var maxCitiesCount = countries.reduce(function (count, country) {
+            return Math.max(count, country.cities.length);
+        }, 0);
 
-    function getPopulationByCountry(countries) {
-        var countryAndPopulation = {};
-
-        for (var country of countries) {
-            countryAndPopulation[country.name] = country.cities.reduce((sum, city) =>
-            sum + city.population, 0);
-        }
-
-        return countryAndPopulation;
+        return countries.filter(function (country) {
+            return country.cities.length === maxCitiesCount;
+        });
     }
 
     var countries = [{
         name: "Brasil",
-        cities: [{ name: "Colombo", population: 177764 },
-                 { name: "RioClaro", population: 1635551 },
-                 { name: "Caxias", population: 133980 },
-                 { name: "Queimados", population: 115020 },
-                 { name: "Apucarana", population: 105114 }]
+        cities: [
+            { name: "Colombo", population: 177764 },
+            { name: "RioClaro", population: 1635551 },
+            { name: "Caxias", population: 133980 },
+            { name: "Queimados", population: 115020 },
+            { name: "Apucarana", population: 105114 }
+        ]
     }, {
         name: "Indonesia",
-        cities: [{ name: "Depok", population: 365200 },
-                 { name: "Pasuruan", population: 134019 },
-                 { name: "Ciputat", population: 270800 },
-                 { name: "Lhokseumawe", population: 109600 }]
+        cities: [
+            { name: "Depok", population: 365200 },
+            { name: "Pasuruan", population: 134019 },
+            { name: "Ciputat", population: 270800 },
+            { name: "Lhokseumawe", population: 109600 }
+        ]
     }, {
         name: "Philippines",
-        cities: [{ name: "Tabaco", population: 107166 },
-                 { name: "Polomolok", population: 110709 },
-                 { name: "Ddagupan", population: 130328 },
-                 { name: "Lipa", population: 218447 },
-                 { name: "Cotobato", population: 163849 }]
+        cities: [
+            { name: "Tabaco", population: 107166 },
+            { name: "Polomolok", population: 110709 },
+            { name: "Ddagupan", population: 130328 },
+            { name: "Lipa", population: 218447 },
+            { name: "Cotobato", population: 163849 }
+        ]
     }, {
         name: "United Kingdom",
-        cities: [{ name: "Oxford", population: 144000 },
-                 { name: "Cambridge", population: 121000 },
-                 { name: "Birmingham", population: 1013000 },
-                 { name: "Glasglow", population: 619680 }]
+        cities: [
+            { name: "Oxford", population: 144000 },
+            { name: "Cambridge", population: 121000 },
+            { name: "Birmingham", population: 1013000 },
+            { name: "Glasglow", population: 619680 }
+        ]
     }];
 
-    console.log("Страна(ы) с максимальным количеством городов:")
-    console.log(getCountriesWithMaxCountOfCities(countries));
+    var countriesPopulation = {};
 
-    console.log("{cтрана: численность}")
-    console.log(getPopulationByCountry(countries));
+    countries.forEach(function (country) {
+        countriesPopulation[country.name] = country.cities.reduce(function (sum, city) {
+            return sum + city.population;
+        }, 0);
+    });
+
+    console.log("Страна(ы) с максимальным количеством городов:");
+    console.log(getCountriesWithCitiesMaxCount(countries));
+
+    console.log("{cтрана: численность}");
+    console.log(countriesPopulation);
 })();
