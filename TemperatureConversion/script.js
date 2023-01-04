436edd8fb0b0b846e52322ea158b4d72c4d0c351
Mikhail Blinov <spasibo1989@gmail.com>
2023-01-04 22:57:38 +0700

    Добавление script.js со скриптом

---
+++ TemperatureConversion/script.js
@@ -0,0 +1,47 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var convertButton = document.querySelector(".converter button");
+    var celsiusInput = document.querySelector(".converter input");
+    var converter = document.querySelector(".converter");
+
+    var result = document.createElement("div");
+
+    converter.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+
+    convertButton.addEventListener("click", function () {
+        celsiusInput.classList.remove("invalid-required", "invalid-number");
+
+        if (celsiusInput.value.trim().length === 0) {
+            celsiusInput.classList.add("invalid-required");
+
+            celsiusInput.value = "";
+            result.innerHTML = "";
+
+            return;
+        }
+
+        var celsiusDegrees = Number(celsiusInput.value);
+
+        if (isNaN(celsiusDegrees)) {
+            celsiusInput.classList.add("invalid-number");
+            
+            result.innerHTML = "";
+
+            return;
+        }
+            
+        result.innerHTML = "<h3>Result:</h3>";
+
+        var kelvinResult = document.createElement("div");
+        var fahrenheitResult = document.createElement("div");
+
+        kelvinResult.textContent = celsiusDegrees + 273.15 + " °K";
+        fahrenheitResult.textContent = celsiusDegrees * 9 / 5 + 32 + " °F";
+
+        result.appendChild(kelvinResult);
+        result.appendChild(fahrenheitResult);
+    });
+
+    converter.appendChild(result);
+});
